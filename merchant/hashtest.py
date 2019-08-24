60290fa603436de0a451421cd6a4dd751e83b8fb
Ubuntu <ubuntu@ip-172-31-81-123.ec2.internal>
2019-08-24 05:56:56 +0000

    adding merchant webpage

---
+++ merchant/hashtest.py
@@ -0,0 +1,35 @@
+import random
+import hashlib
+from hashlib import sha256
+import time
+# Generate a signature / verification key pair.
+from petlib.ec import EcGroup
+from petlib.ecdsa import do_ecdsa_sign, do_ecdsa_verify 
+from aux_functions import savekey, readkey, pack, unpack 
+#files
+HASH_BILL = 'hash_bill.txt'
+SIGNATURE = 'signature3.txt'
+
+#code
+G = EcGroup()
+sig_key = unpack(readkey('sig_key.txt'))
+print(sig_key)
+
+ver_key = unpack(readkey('ver_key.txt'))
+print('esto es ver key',ver_key)
+time.sleep(3)
+
+bill_number = str(random.randint(1, 10000))
+bill_hashed = hashlib.sha256(bill_number.encode('utf-8')).digest()
+
+#bill_number = sha1(b'12345').digest()
+print bill_number
+savekey(HASH_BILL, pack(bill_hashed))
+time.sleep(3)
+print unpack(readkey('hash_bill.txt'))
+# Sign and verify signature
+signature_bill = do_ecdsa_sign(G, sig_key, bill_hashed)
+savekey(SIGNATURE, pack(signature_bill))
+time.sleep(3)
+sig = unpack(readkey('signature3.txt'))
+assert do_ecdsa_verify(G, ver_key, sig, bill_hashed)
